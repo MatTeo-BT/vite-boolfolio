abd9e95f69c3785e65a1f0b89b392b97de7167eb
MatTeo-BT <aboutmet@gmail.com>
2024-02-26 21:13:27 +0100

    esercizio completo

---
+++ vite-boolfolio/src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppHeader',
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ vite-boolfolio/src/components/AppMain.vue
@@ -0,0 +1,43 @@
+<template lang="">
+    <div>
+       <ul>
+        <li v-for="project in projects" :key='project.id'>
+            {{project.title}}
+        </li>
+       </ul>
+    </div>
+</template>
+<script>
+import axios from 'axios';
+export default {
+    name: 'AppMain',
+    data() {
+        return {
+            projects: [
+
+            ],
+        }
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                }
+            })
+                .then((response) => {
+                    console.log(response.data.data);
+                    this.projects = response.data.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+    },
+    created() {
+        this.getProjects();
+    }
+}
+</script>
+<style lang="">
+    
+</style>
